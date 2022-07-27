8170ba48cb0b249cd4cc70cb34d2ac43868c4be2
Tiago Caceraghi dos Santos <tiago.caceraghi@edu.sc.senai.br>
2022-07-27 11:49:01 -0300

    VERSAO 0.0.4 REPETICAO DO CALCULO DA MÉDIA

---
+++ for.js
@@ -0,0 +1,16 @@
+//Exemplo de Estrutura de repetição com FOR
+/*
+for (contador = 1; contador <= 10; contador++){
+    //informar o que eu desejo repetir
+    console.log("Repetição..: " +contador);
+}
+*/
+console.log("Tabuada da Multiplicação do 2");
+for (tabu = 1; tabu <= 10; tabu++){
+    //comando a ser repetido
+    console.log("2 x "+tabu+" = "+2*tabu);
+}
+
+for (x = 0; x < 10; x++){
+    console.log("Eu faço a minha!!");
+}
