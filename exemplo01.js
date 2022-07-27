fa4742c638249ee22167c54c74f8b3debe722b2f
Tiago Caceraghi dos Santos <tiago.caceraghi@edu.sc.senai.br>
2022-07-27 10:47:43 -0300

    VERSAO 0.0.2 Exemplo01.js - Processamento e Saída

--- exemplo01.js
+++ exemplo01.js
@@ -1,8 +1,23 @@
+//Declaração das Variáveis onde vamos guardar os valores
 var nota1, nota2, nota3, media;
 
+//Importação da biblioteca realine-sync para pegar os dados
+//digitados no teclado
 var entrada = require('readline-sync');
+
+//Solicitando e pegando as notas
+//OBS: parseFloat faz a conversão de um tipo texto para numérico(Float)
 nota1 = parseFloat(entrada.question("Informe a Primeira Nota..: "));
 nota2 = parseFloat(entrada.question("Informe a Segunda Nota...: "));
 nota3 = parseFloat(entrada.question("Informe a Terceira Nota..: "));
 
-console.log(nota1, nota2, nota3);
+//processamento- Cálculo da Média
+media = (nota1+nota2+nota3)/3;
+//saída: exibindo a média do aluno
+console.log("Média..: "+media);
+
+//Estrutura de Decisão - Condição
+if (media >= 7)
+    console.log("APROVADOOOOO!");
+else
+    console.log("Reprovado");
