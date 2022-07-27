e16a546f9a3d36b76c5a796ea750e9f07b39029c
Tiago Caceraghi dos Santos <tiago.caceraghi@edu.sc.senai.br>
2022-07-27 10:18:58 -0300

    Versão 0.0.1 do exemplo01.js

---
+++ exemplo01.js
@@ -0,0 +1,8 @@
+var nota1, nota2, nota3, media;
+
+var entrada = require('readline-sync');
+nota1 = parseFloat(entrada.question("Informe a Primeira Nota..: "));
+nota2 = parseFloat(entrada.question("Informe a Segunda Nota...: "));
+nota3 = parseFloat(entrada.question("Informe a Terceira Nota..: "));
+
+console.log(nota1, nota2, nota3);
